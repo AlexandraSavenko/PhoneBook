97277af6694ad79b696a168e738567650f3ce823
Sasha Savenko <sashasavenko3@gmail.com>
2025-05-14 18:44:28 +0300

    init commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store.js";
-import App from "./App.jsx";
+import App from "./App.js";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")as HTMLElement).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>

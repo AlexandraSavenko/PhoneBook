ecacea69941e0e58d9995cd32f6c4cf5d1187ce3
Sasha Savenko <sashasavenko3@gmail.com>
2025-05-25 11:48:58 +0300

    regester form css added

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -3,14 +3,19 @@ import css from "./RegisterForm.module.css";
 import { register } from "../../redux/auth/operations";
 import { Field, Form, Formik, FormikHelpers } from "formik";
 import { AppDispatch } from "../../redux/store";
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 export interface RegFormValues {
-  email: string,
+email: string,
 name: string,
 password: string,
 }
 
 export default function RegisterForm() {
+
+  const [visible, setVisible] = useState(false)
+  
   const dispatch = useDispatch<AppDispatch>();
   const handleSubmit = (values: RegFormValues, actions: FormikHelpers<RegFormValues>): void => {
     dispatch(register(values));
@@ -19,21 +24,24 @@ export default function RegisterForm() {
   return (
     <Formik
       initialValues={{ name: "", email: "", password: "" }}
-      onSubmit={handleSubmit}
-    >
+      onSubmit={handleSubmit}>
       <Form className={css.form}>
-        <label htmlFor="name">
-          Usarname
-        </label>
-        <Field type="text" name="name" />
-        <label htmlFor="email">
-          Email
-        </label>
-        <Field type="email" name="email" />
-        <label htmlFor="password">
-          Password
-        </label>
-        <Field type="password" name="password" />
+        <div className={css.inputwrap}>
+        
+        <Field type="text" name="name" className={css.input} placeholder=" " autocomplete="off" />
+        <label htmlFor="name" className={css.label}>Usarname</label>
+        </div>
+        <div className={css.inputwrap}>
+        
+        <Field type="email" name="email" className={css.input} placeholder=" " autocomplete="off" />
+        <label htmlFor="email" className={css.label}>Email</label>
+        </div>
+        <div className={css.inputwrap}>
+        
+        <Field type={visible ? "text" : "password"} name="password" className={css.input} placeholder=" " autocomplete="off"/>
+        <label htmlFor="password" className={css.label}>Password</label>
+        <span className={css.eye} onClick={() => setVisible(!visible)} >{visible ? <FaEyeSlash/> : <FaEye/>}</span>
+        </div>
         <button type="submit">Register</button>
       </Form>
     </Formik>

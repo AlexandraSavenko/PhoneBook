b3e3c0da2a21304cb23a30910e08ad2eaef706ce
Sasha Savenko <sashasavenko3@gmail.com>
2025-05-25 12:43:43 +0300

    * added

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -19,8 +19,8 @@ export default function RegisterForm() {
   const [visible, setVisible] = useState(false)
 
   const FormSchema = Yup.object().shape({
-    name: Yup.string().required("Required"),
-    email: Yup.string().email("Must be a valid email!").required("Required"),
+    name: Yup.string().required(""),
+    email: Yup.string().email("Must be a valid email!").required(""),
     password: Yup.string()
     .min(8, 'Password must be 8 characters long')
     .matches(/[0-9]/, 'Password requires a number')
@@ -44,17 +44,17 @@ export default function RegisterForm() {
       <Form className={css.form}>
         <div className={css.inputwrap}>
         <Field type="text" name="name" className={css.input} placeholder=" " autoComplete="off" />
-        <label htmlFor="name" className={css.label}>Usarname</label>
+        <label htmlFor="name" className={css.label}>Usarname *</label>
         {errors.name && <p className={css.warning}>{errors.name}</p>}
         </div>
         <div className={css.inputwrap}>
         <Field type="email" name="email" className={css.input} placeholder=" " autoComplete="off" />
-        <label htmlFor="email" className={css.label}>Email</label>
+        <label htmlFor="email" className={css.label}>Email *</label>
         {errors.email && <p className={css.warning}>{errors.email}</p>}
         </div>
         <div className={css.inputwrap}>
         <Field type={visible ? "text" : "password"} name="password" className={css.input} placeholder=" " autoComplete="off"/>
-        <label htmlFor="password" className={css.label}>Password</label>
+        <label htmlFor="password" className={css.label}>Password *</label>
         <span className={css.eye} onClick={() => setVisible(!visible)} >{visible ? <FaEyeSlash/> : <FaEye/>}</span>
         {errors.password ? <p className={css.warning}>{errors.password}</p> : <p className={css.warning}>The password must be 8+ characters long, including at least one uppercase letter, one number and one symbol</p> }
         </div>

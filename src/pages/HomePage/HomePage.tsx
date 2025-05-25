6791c058d476146d1790f7536dfa0d843c944894
Sasha Savenko <sashasavenko3@gmail.com>
2025-05-25 14:25:59 +0300

    h1 css added

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,9 @@
+import css from './HomePage.module.css'
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Phonebook</h1>
+      <h1 className={css.header}>Phone<span>book</span></h1>
     </div>
   );
 }

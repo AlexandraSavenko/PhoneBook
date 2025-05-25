642a28e0a1b16771568281edf1f0e4f7e4147132
Sasha Savenko <sashasavenko3@gmail.com>
2025-05-25 11:58:28 +0300

    yup validation added

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -5,6 +5,8 @@ import { Field, Form, Formik, FormikHelpers } from "formik";
 import { AppDispatch } from "../../redux/store";
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import * as Yup from "yup";
+
 
 export interface RegFormValues {
 email: string,
@@ -15,6 +17,18 @@ password: string,
 export default function RegisterForm() {
 
   const [visible, setVisible] = useState(false)
+
+  const FormSchema = Yup.object().shape({
+    name: Yup.string().required(),
+    email: Yup.string().email("Must be a valid email!").required("Required"),
+    password: Yup.string()
+    .min(8, 'Password must be 8 characters long')
+    .matches(/[0-9]/, 'Password requires a number')
+    .matches(/[a-z]/, 'Password requires a lowercase letter')
+    .matches(/[A-Z]/, 'Password requires an uppercase letter')
+    .matches(/[^\w]/, 'Password requires a symbol'),
+  });
+
   
   const dispatch = useDispatch<AppDispatch>();
   const handleSubmit = (values: RegFormValues, actions: FormikHelpers<RegFormValues>): void => {
@@ -24,26 +38,29 @@ export default function RegisterForm() {
   return (
     <Formik
       initialValues={{ name: "", email: "", password: "" }}
+      validationSchema={FormSchema}
       onSubmit={handleSubmit}>
+        {({ errors }) => (
       <Form className={css.form}>
         <div className={css.inputwrap}>
-        
         <Field type="text" name="name" className={css.input} placeholder=" " autocomplete="off" />
         <label htmlFor="name" className={css.label}>Usarname</label>
+        {errors.name && <p>{errors.name}</p>}
         </div>
         <div className={css.inputwrap}>
-        
         <Field type="email" name="email" className={css.input} placeholder=" " autocomplete="off" />
         <label htmlFor="email" className={css.label}>Email</label>
+        {errors.email && <p>{errors.email}</p>}
         </div>
         <div className={css.inputwrap}>
-        
         <Field type={visible ? "text" : "password"} name="password" className={css.input} placeholder=" " autocomplete="off"/>
         <label htmlFor="password" className={css.label}>Password</label>
         <span className={css.eye} onClick={() => setVisible(!visible)} >{visible ? <FaEyeSlash/> : <FaEye/>}</span>
+        {errors.password && <p>{errors.password}</p>}
         </div>
         <button type="submit">Register</button>
       </Form>
+        )}
     </Formik>
   );
 }
